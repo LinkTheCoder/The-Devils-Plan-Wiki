b90efb8bb272e121851a32134319a9b704a85a1d
LinkTheCoder <66062268+LinkTheCoder@users.noreply.github.com>
2024-04-29 11:48:08 +0200

    update site to the devils plan

--- docs/intro.md
+++ docs/intro.md
@@ -1,47 +1,17 @@
 ---
 sidebar_position: 1
+sidebar_label: '-- SEASON 1 --'
 ---
 
-# Tutorial Intro
+# SYNOPSYS
 
-Let's discover **Docusaurus in less than 5 minutes**.
+Twelve celebrity contestants are initially given one "Piece". A player with no Pieces is eliminated; Pieces can be traded between players or spent on in-game benefits. Pieces can be won and lost in the two daily matches: the Main Match and the Prize Match.
 
-## Getting Started
+After the competitive Main Match, the two surviving players with the fewest Pieces are sent to prison until the next Main Match. The player or players with the most Pieces make tie-breaking decisions about who is sent to prison. The collaborative Prize Match determines whether a sum of money—₩50,000,000 or ₩100,000,000—is added to the final pot, which can total up to ₩500,000,000. [1]
 
-Get started by **creating a new site**.
+Games featured include social deduction games, board games, card games and abstract strategy games like nine men's morris. Main Matches typically see players forming alliances, while the Prize Matches test skills including memorization, numerical calculation and visualization.
 
-Or **try Docusaurus immediately** with **[docusaurus.new](https://docusaurus.new)**.
+Combining three different types of pieces forms an octahedron-like shape from which the clue "NEXT YEAR" can be read. Using this clue—by typing 2024 into a hidden safe in the prison—unlocks a special individual challenge - a variant of Gomoku in which the player must remember the colour of each piece, in which the player can win 10 Pieces but risk elimination from the game.
 
-### What you'll need
-
-- [Node.js](https://nodejs.org/en/download/) version 18.0 or above:
-  - When installing Node.js, you are recommended to check all checkboxes related to dependencies.
-
-## Generate a new site
-
-Generate a new Docusaurus site using the **classic template**.
-
-The classic template will automatically be added to your project after you run the command:
-
-```bash
-npm init docusaurus@latest my-website classic
-```
-
-You can type this command into Command Prompt, Powershell, Terminal, or any other integrated terminal of your code editor.
-
-The command also installs all necessary dependencies you need to run Docusaurus.
-
-## Start your site
-
-Run the development server:
-
-```bash
-cd my-website
-npm run start
-```
-
-The `cd` command changes the directory you're working with. In order to work with your newly created Docusaurus site, you'll need to navigate the terminal there.
-
-The `npm run start` command builds your website locally and serves it through a development server, ready for you to view at http://localhost:3000/.
-
-Open `docs/intro.md` (this page) and edit some lines: the site **reloads automatically** and displays your changes.
+> References
+> 1. [The Devil's Plan Episodes 5-9: Explaining the games](https://www.dexerto.com/tv-movies/the-devils-plan-games-explained-2310672/). Dexerto. Retrieved 2023-10-06.
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,20 +3,20 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'Docusaurus',
-  tagline: 'Dinosaurs are cool',
+  title: "The Devil's Plan Wiki",
+  tagline: 'Solving the mystery of the devil',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://thedevilsplan-wiki.vercel.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'LinkTheCoder', // Usually your GitHub org/user name.
+  projectName: 'The-Devils-Plan-Wiki', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -38,14 +38,14 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/LinkTheCoder/The-Devils-Plan-Wiki/tree/main',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/LinkTheCoder/The-Devils-Plan-Wiki/tree/main',
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -55,27 +55,26 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
+
+    colorMode: {
+      defaultMode: 'dark',
+    },
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: "THE DEVIL'S PLAN WIKI",
       logo: {
         alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        src: 'img/logo.png',
+        srcDark: 'img/logo_dark.png',
       },
       items: [
         {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Docs',
         },
         {to: '/blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
-        },
       ],
     },
     footer: {
@@ -91,19 +90,15 @@ const config: Config = {
           ],
         },
         {
-          title: 'Community',
+          title: 'Socials',
           items: [
             {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              label: 'Official Website',
+              href: 'https://www.devilsplan.co.kr/',
             },
             {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
+              label: 'Netflix',
+              href: 'https://www.netflix.com/title/81653386',
             },
           ],
         },
@@ -121,7 +116,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Website made with 💜 by Link Häggman.`,
     },
     prism: {
       theme: prismThemes.github,
